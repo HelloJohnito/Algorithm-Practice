2330d1928c3b48314e3d0a4015dbc3d61c7b9382
John <johnitolee@gmail.com>
2017-04-17 10:59:19 -0700

    add heap_sort

--- Heap/lib/heap.rb
+++ Heap/lib/heap.rb
@@ -11,7 +11,7 @@ class BinaryMinHeap
     @store[0], @store[-1] = @store[-1], @store[0]
     value = @store.pop
     BinaryMinHeap.heapify_down(@store, 0)
-    value 
+    value
   end
 
   def peek
@@ -51,32 +51,38 @@ class BinaryMinHeap
 
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    # prc ||= Proc.new{ |el1, el2| el1 <=> el2 }
+
     children_indices = BinaryMinHeap.child_indices(len, parent_idx)
 
-    if children_indices.empty?
-      return array
-    elsif children_indices.length == 1
-      child_one_idx = children_indices[0]
+
+    return array if children_indices.empty?
+
+
+    if children_indices.length == 1
+      child_idx = children_indices[0]
     else
-      child_one_idx, child_two_idx = children_indices
+      if array[children_indices[0]] > array[children_indices[1]]
+        child_idx = children_indices[1]
+      else
+        child_idx = children_indices[0]
+      end
     end
 
-    # Compares the parent node to its child_nodes.
-    if array[parent_idx] > array[child_one_idx]
-      array[parent_idx], array[child_one_idx] = array[child_one_idx], array[parent_idx]
-      parent_idx = child_one_idx
-      self.heapify_down(array, parent_idx)
-    elsif !child_two_idx.nil? && array[parent_idx] > array[child_two_idx]
-      array[parent_idx], array[child_two_idx] = array[child_two_idx], array[parent_idx]
-      parent_idx = child_two_idx
-      self.heapify_down(array, parent_idx)
+    if array[parent_idx] > array[child_idx]
+      array[parent_idx], array[child_idx] = array[child_idx], array[parent_idx]
+      parent_idx = child_idx
+      self.heapify_down(array, parent_idx, len)
     end
 
     return array
   end
 
 
+
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+
+    return if array[child_idx] == nil
     return array if child_idx == 0
     parent_idx = BinaryMinHeap.parent_index(child_idx)
 
@@ -89,3 +95,22 @@ class BinaryMinHeap
     return array
   end
 end
+
+
+
+
+
+  # def self.heapify_down(array, parent_idx, len = array.length, &prc)
+  #   prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+  #   children = self.child_indices(len, parent_idx)
+  #   return array if children.all? { |idx| prc.call(array[parent_idx], array[idx]) <= 0 }
+  #   if children.length == 1
+  #     largest = children.first
+  #   else
+  #     child_values = [array[children[0]], array[children[1]]]
+  #     largest = prc.call(child_values[0], child_values[1]) == -1 ?
+  #       children[0] : children[1]
+  #   end
+  #   array[parent_idx], array[largest] = array[largest], array[parent_idx]
+  #   self.heapify_down(array, largest, len, &prc)
+  # end
--- Heap/lib/heap_sort.rb
+++ Heap/lib/heap_sort.rb
@@ -2,5 +2,22 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+
+    i = 1
+    while i <= self.length
+      BinaryMinHeap.heapify_up(self, i)
+      i += 1
+    end
+
+    j = self.length
+    while j != 0
+      # puts j
+      self[j-1], self[0] = self[0], self[j-1]
+      j -= 1
+      BinaryMinHeap.heapify_down(self, 0, j)
+    end
+
+    return self.reverse!
+
   end
 end
