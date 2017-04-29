ede3659e73ae16959f6dcee4b30cb8bc1bcbc693
John <johnitolee@gmail.com>
2017-04-28 21:56:31 -0700

    add questions

---
+++ Questions/interview1.js
@@ -0,0 +1,518 @@
+
+// stockPricesYesterday buy first then sell. Find the max profit.
+//o(1) time with o(1) space complexity
+//Greedy methodology
+
+function getMaxProfit(array){
+  let minPrice = array[0];
+  let maxProfit = array[1] - array[0];
+
+  for(let i = 1; i < array.length; i++){
+    let currentPrice = array[i];
+    let potentialPrice = currentPrice - minPrice;
+    minPrice = Math.min(minPrice, currentPrice);
+    maxProfit = Math.max(potentialPrice, maxProfit);
+  }
+  return maxProfit;
+}
+
+// var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+// var stockPricesYesterday = [12,11,10]
+
+////////////////////////////////////////////////////////////
+
+//You have an array of integers, and for each index you want to find the product of every integer except the integer at that index. Don't use division.
+
+function getProductsOfAllIntsExceptAtIndex(array){
+  let result = [];
+
+  let lowerProduct = 1;
+  for(let j = 0; j < array.length; j++){
+    result[j] = lowerProduct;
+    lowerProduct *= array[j];
+  }
+
+  let upperProduct = 1;
+  for(let k = array.length - 1; k >= 0 ; k--){
+    result[k] *= upperProduct;
+    upperProduct *= array[k];
+  }
+
+  return result;
+}
+
+// getProductsOfAllIntsExceptAtIndex([1, 7, 3, 4]);
+
+///////////////////////////////////////////////////////////
+
+// Given an array of integers, find the highest product you can get from three of the integers.
+
+//o(nlogn) solution but does not account for negative numbers
+  //my Naive answer//
+function productThree(arrayOfInt){
+  arrayOfInt.sort();
+  let arrayLengthThree = arrayOfInt.slice(arrayOfInt.length - 3, arrayOfInt.length);
+  return multiply(arrayLengthThree);
+}
+
+function multiply (numbers){
+  let product = 1;
+  for(let i = 0; i < numbers.length; i++){
+    product *= numbers[i];
+  }
+  return product;
+}
+
+
+//o(n) solution with o(1) memory
+
+function productThree(arrayOfInt){
+  let productMaxThree = arrayOfInt[0] * arrayOfInt[1] * arrayOfInt[2];
+  let highestTwo = arrayOfInt[0] * arrayOfInt[1];
+  let lowestTwo = arrayOfInt[0] * arrayOfInt[1];
+  let highest = Math.max(arrayOfInt[0], arrayOfInt[1]);
+  let lowest = Math.min(arrayOfInt[0], arrayOfInt[1]);
+  let current;
+
+  for(let i = 2; i < arrayOfInt.length; i++){
+    current = arrayOfInt[i];
+
+    //set productMaxThree
+    productMaxThree = Math.max(
+      productMaxThree,
+      current * highestTwo,
+      current * lowestTwo
+    );
+
+    //set product twos
+    highestTwo = Math.max(
+      highestTwo,
+      current * highest,
+      current * lowest
+    );
+
+    lowestTwo = Math.min(
+      lowestTwo,
+      current * highest,
+      current * lowest
+    );
+
+    //set product ones
+    highest = Math.max(
+      highest,
+      current
+    );
+
+    lowest = Math.min(
+      lowest,
+      current
+    );
+  }
+
+  return productMaxThree;
+}
+
+/////////////////////////////////////////////////////////
+
+// Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.
+
+// o(nlog(n))
+function mergeRanges(meetings) {
+
+  let sortedMeetings = meetings.slice().sort(
+    (a,b) => a.startTime > b.startTime ? 1 : -1
+  );
+
+  let answer = [];
+  let currentMeeting = sortedMeetings[0];
+
+  for(let i = 1; i < meetings.length; i++){
+    if(currentMeeting.endTime >= meetings[i].startTime){
+      currentMeeting = merge(currentMeeting, meetings[i]);
+    } else {
+      answer.push(currentMeeting);
+      currentMeeting = meetings[i];
+    }
+  }
+  answer.push(currentMeeting);
+  return answer;
+}
+
+function merge(meetingOne, meetingTwo){
+  return {
+    startTime: Math.min(meetingOne.startTime, meetingTwo.startTime),
+    endTime: Math.max(meetingOne.endTime, meetingTwo.endTime)
+  };
+}
+
+// mergeRanges(times)
+
+// let times = [
+//   {startTime: 0,  endTime: 1},
+//   {startTime: 3,  endTime: 5},
+//   {startTime: 4,  endTime: 8},
+//   {startTime: 10, endTime: 12},
+//   {startTime: 9,  endTime: 10},
+// ]
+
+//InterviewCake Solution
+function mergeRanges(meetings) {
+
+  // sort by start times
+  var sortedMeetings = meetings.slice().sort(function(a, b) {
+      return a.startTime > b.startTime ? 1 : -1;
+  });
+
+  // initialize mergedMeetings with the earliest meeting
+  var mergedMeetings = [sortedMeetings[0]];
+
+  for (var i = 1; i < sortedMeetings.length; i++) {
+
+      var currentMeeting    = sortedMeetings[i];
+      var lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+      // if the current and last meetings overlap, use the latest end time
+      if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+          lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime);
+
+      // add the current meeting since it doesn't overlap
+      } else {
+          mergedMeetings.push(currentMeeting);
+      }
+  }
+
+  return mergedMeetings;
+}
+///////////////////////////////////////////////////////////////////
+
+
+//Find the area of the overlap between two rectangle
+
+function findRangeOverlap(point1, length1, point2, length2) {
+
+  // find the highest start point and lowest end point.
+  // the highest ("rightmost" or "upmost") start point is
+  // the start point of the overlap.
+  // the lowest end point is the end point of the overlap.
+  var highestStartPoint = Math.max(point1, point2);
+  var lowestEndPoint = Math.min(point1 + length1, point2 + length2);
+
+  // return null overlap if there is no overlap
+  if (highestStartPoint >= lowestEndPoint) {
+      return {startPoint: null, overlapLength: null};
+  }
+
+  // compute the overlap length
+  var overlapLength = lowestEndPoint - highestStartPoint;
+
+  return {startPoint: highestStartPoint, overlapLength: overlapLength};
+}
+
+function findRectangularOverlap(rect1, rect2) {
+
+  // get the x and y overlap points and lengths
+  var xOverlap = findRangeOverlap(rect1.leftX, rect1.width, rect2.leftX, rect2.width);
+  var yOverlap = findRangeOverlap(rect1.bottomY, rect1.height, rect2.bottomY, rect2.height);
+
+  // return null rectangle if there is no overlap
+  if (!xOverlap.overlapLength || !yOverlap.overlapLength) {
+      return {
+          leftX: null,
+          bottomY: null,
+          width: null,
+          height: null,
+      };
+  }
+
+  return {
+      leftX: xOverlap.startPoint,
+      bottomY: yOverlap.startPoint,
+      width: xOverlap.overlapLength,
+      height: yOverlap.overlapLength,
+  };
+}
+
+
+// var rect1 = {
+//
+//   // coordinates of bottom-left corner
+//   leftX: 2,
+//   bottomY: 1,
+//
+//   // width and height
+//   width: 4,
+//   height: 4,
+//
+// };
+//
+// var rect2 = {
+//
+//   // coordinates of bottom-left corner
+//   leftX: 1,
+//   bottomY: 2,
+//
+//   // width and height
+//   width: 4,
+//   height: 4,
+//
+// };
+
+
+
+///////////////////////////////////////////////////////////
+
+
+class temperature{
+  constructor(){
+    this.temp = {};
+    this.max = Number.NEGATIVE_INFINITY;
+    this.min = Number.POSITIVE_INFINITY;
+
+    this.mode = 0;
+    this.mostCount = 1;
+
+    this.mean = 0;
+    this.count = 0;
+    this.totalSum = 0;
+  }
+
+  insert(el){
+    this.temp[el] = this.temp.hasOwnProperty(el) ? this.temp[el]+=1 : 1;
+
+    this.max = Math.max(this.max, el);
+    this.min = Math.min(this.min, el);
+
+    this.count += 1;
+    this.totalSum += el;
+    this.mean = this.totalSum/this.count;
+    // let average = 0
+    // let value = 0
+    // for(let key in this.temp){
+    //   average += Number(key) * this.temp[key]
+    //   value += this.temp[key]
+    // }
+    // this.mean = average/value
+
+    // this.mode = Object.keys(this.temp).reduce((a, b) => {
+    //   return this.temp[a] > this.temp[b] ? a : b;
+    // });
+
+    if(this.mostCount <= this.temp[el]){
+      this.mode = el;
+      this.mostCount = this.temp[el];
+    }
+
+
+    return el;
+  }
+
+  getMax(){
+    return this.max;
+  }
+
+  getMin(){
+    return this.min;
+  }
+
+  getMean(){
+    return this.mean;
+  }
+
+  getMode(){
+    return this.mode;
+  }
+
+  getTemp(){
+    return this.temp;
+  }
+}
+
+// q = new temperature
+// q.insert(1)
+// q.insert(2)
+// q.insert(2)
+// q.insert(3)
+// console.log(`max: ${q.getMax()}`)
+// console.log(`min: ${q.getMin()}`)
+// console.log(`mean: ${q.getMean()}`)
+// console.log(`mode: ${q.getMode()}`)
+// console.log(q.max)
+
+
+
+//Binary Tree Balanced?
+
+// function BinaryTreeNode(value) {
+//     this.value = value;
+//     this.left  = null;
+//     this.right = null;
+// }
+//
+// BinaryTreeNode.prototype.insertLeft = function(value) {
+//     this.left = new BinaryTreeNode(value);
+//     return this.left;
+// };
+//
+// BinaryTreeNode.prototype.insertRight = function(value) {
+//     this.right = new BinaryTreeNode(value);
+//     return this.right;
+// };
+//
+// a = new BinaryTreeNode(10)
+//
+// a.insertLeft(5)
+// b = a.left
+// b.insertLeft(3)
+// b.insertRight(7)
+//
+// d = b.left
+// d.insertLeft(1)
+//
+// a.insertRight(15)
+// c = a.right
+// c.insertLeft(13)
+// c.insertRight(17)
+
+
+
+function balancedTree(tree){
+
+  if(!tree) return true;
+
+  let depths = [];
+  //stack
+  let nodes = [[tree, 0]];
+
+  while(nodes.length){
+
+    let nodeInfo = nodes.pop();
+    let node = nodeInfo[0];
+    let depth = nodeInfo[1];
+
+    //when at leafnode
+    if(!node.left && !node.right){
+      if(depths.indexOf(depth) < 0){
+       depths.push(depth);
+        if(depths.length > 2 || Math.abs(depths[0] - depths[1]) > 1){
+          return false;
+        }
+      }
+    }
+
+    else{
+      if(node.right) nodes.push([node.right, depth + 1]);
+      if(node.left) nodes.push([node.left, depth + 1]);
+    }
+  }
+  return true;
+}
+
+// balancedTree(a)
+
+
+///////////////////////////////////////////////////////////////////////
+
+//Check if tree is a binary search tree
+
+function isBinarySearchTree(tree){
+  let stack = [];
+
+  stack.push({node: tree, lowerBound: -Infinity, upperBound: Infinity});
+
+  while(stack.length){
+    let nodeAndBound = stack.pop();
+    let currentNode = nodeAndBound.node;
+    let lowerBound = nodeAndBound.lowerBound;
+    let upperBound = nodeAndBound.upperBound;
+
+    if(currentNode.value <= lowerBound || currentNode.value >= upperBound){
+      return false;
+    }
+
+    if(currentNode.left){
+      stack.push({node: currentNode.left, lowerBound: lowerBound, upperBound: currentNode.value});
+    }
+    if(currentNode.right){
+      stack.push({node: currentNode.right, lowerBound: currentNode.value, upperBound: upperBound});
+    }
+  }
+  return true;
+}
+
+// isBinarySearchTree(a)
+
+function recursiveIsBinarySearchTree(treeRoot, lowerBound, upperBound) {
+
+  lowerBound = (typeof lowerBound !== 'undefined') ? lowerBound : -Infinity;
+  upperBound = (typeof upperBound !== 'undefined') ? upperBound :  Infinity;
+
+  if (!treeRoot) return true;
+
+  if (treeRoot.value >= upperBound || treeRoot.value <= lowerBound) {
+      return false;
+  }
+
+  return isBinarySearchTree(treeRoot.left, lowerBound, treeRoot.value) &&
+         isBinarySearchTree(treeRoot.right, treeRoot.value, upperBound);
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+//2nd largest element in a binary search tree
+
+//my solution
+function secondLargest(tree){
+  let previousNode = null;
+  let currentNode = tree;
+
+  while(currentNode.right !== null){
+    previousNode = currentNode;
+    currentNode = currentNode.right;
+  }
+
+  if(currentNode.left){
+    currentNode = currentNode.left;
+    while(currentNode.right !== null){
+      currentNode = currentNode.right;
+    }
+    return currentNode.value;
+  }
+
+  return previousNode.value;
+}
+
+//interview solution
+function findLargest(rootNode) {
+    var current = rootNode;
+    while (current) {
+        if (!current.right) return current.value;
+        current = current.right;
+    }
+}
+
+function findSecondLargest(rootNode) {
+    if (!rootNode || (!rootNode.left && !rootNode.right)) {
+        throw new Error('Tree must have at least 2 nodes');
+    }
+
+    var current = rootNode;
+
+    while (current) {
+        // case: current is largest and has a left subtree
+        // 2nd largest is the largest in that subtree
+        if (current.left && !current.right) {
+            return findLargest(current.left);
+        }
+
+        // case: current is parent of largest, and largest has no children,
+        // so current is 2nd largest
+        if (current.right &&
+                !current.right.left &&
+                !current.right.right) {
+            return current.value;
+        }
+
+        current = current.right;
+    }
+}
---
+++ Questions/interview2.js
@@ -0,0 +1,194 @@
+//Web crawler, how to save space. Instead of using a set what can you use to save storage.
+function webCrawlerNewStorage(word, startRoot){
+  let root = startRoot || {};
+  let currentNode = root;
+  let isNewWord = false;
+  for(let i = 0; i < word.length; i++){
+    let char = word[i];
+
+    if (!currentNode.hasOwnProperty(char)){
+      isNewWord = true;
+      currentNode[char] = {};
+    }
+
+    currentNode = currentNode[char];
+  }
+
+  if(!currentNode.hasOwnProperty("end")){
+    isNewWord = true;
+    currentNode["end"] = {};
+  }
+  return isNewWord;
+}
+
+// console.log(webCrawlerNewStorage("hi",
+//  {"h": {"i": { "end":{} }}} ));
+// console.log(webCrawlerNewStorage("google"));
+
+
+////////////////////////////////////////////////////////////
+
+// Find the index of the beginning element of this rotated array.
+
+//my solution
+function dictionaryBinarySearch(array, front){
+  let first = front || array[0];
+  let midIndex = Math.floor(array.length / 2);
+  let currentEl = array[midIndex];
+  let leftEl = array[midIndex - 1];
+
+  if(leftEl > currentEl || leftEl === undefined){
+    return midIndex;
+  }
+  else if(currentEl < first){
+    return dictionaryBinarySearch(array.slice(0, midIndex), first);
+  }
+  else if(currentEl >= first){
+    return dictionaryBinarySearch(array.slice(midIndex + 1, array.length), midIndex, first) + midIndex + 1;
+  }
+}
+
+
+// interview solution
+
+function findRotationPoint(words) {
+  const firstWord = words[0];
+
+  var floorIndex = 0;
+  var ceilingIndex = words.length - 1;
+
+  while (floorIndex < ceilingIndex) {
+
+      // guess a point halfway between floor and ceiling
+      var guessIndex = Math.floor(floorIndex + ((ceilingIndex - floorIndex) / 2));
+
+      // if guess comes after first word or is the first word
+      if (words[guessIndex] >= firstWord) {
+          // go right
+          floorIndex = guessIndex;
+      } else {
+          // go left
+          ceilingIndex = guessIndex;
+      }
+
+      // if floor and ceiling have converged
+      if (floorIndex + 1 === ceilingIndex) {
+
+          // between floor and ceiling is where we flipped to the beginning
+          // so ceiling is alphabetically first
+          break;
+      }
+  }
+
+  return ceilingIndex;
+}
+
+
+/////////////////////////////////////////////////////////////////////
+// two sum
+
+function canTwoMoviesFillFlight(movieLengths, flightLength) {
+
+  // movie lengths we've seen so far
+  var movieLengthsSeen = new Set();
+
+  for (var i = 0; i < movieLengths.length; i++) {
+      var firstMovieLength = movieLengths[i];
+
+      var matchingSecondMovieLength = flightLength - firstMovieLength;
+      if (movieLengthsSeen.has(matchingSecondMovieLength)) {
+          return true;
+      }
+
+      movieLengthsSeen.add(firstMovieLength);
+  }
+
+  // we never found a match, so return false
+  return false;
+}
+
+//////////////////////////////////////////////////////////////////////
+//nth fib non recursive
+
+
+function fib(n) {
+
+  // edge cases:
+  if (n < 0) {
+      throw new Error('Index was negative. No such thing as a negative index in a series.');
+  } else if (n === 0 || n === 1) {
+      return n;
+  }
+
+  // we'll be building the fibonacci series from the bottom up
+  // so we'll need to track the previous 2 numbers at each step
+  var prevPrev = 0;  // 0th fibonacci
+  var prev = 1;      // 1st fibonacci
+  var current;       // Declare current
+
+  for (var i = 1; i < n; i++) {
+      // Iteration 1: current = 2nd fibonacci
+      // Iteration 2: current = 3rd fibonacci
+      // Iteration 3: current = 4th fibonacci
+      // To get nth fibonacci ... do n-1 iterations.
+      current = prev + prevPrev;
+      prevPrev = prev;
+      prev = current;
+  }
+
+  return current;
+}
+
+////////////////////////////////////////////////////////////////////////
+//knapsack
+
+function maxDuffelBagValue(cakeTypes, weightCapacity) {
+
+  // we make an array to hold the maximum possible value at every
+  // duffel bag weight capacity from 0 to weightCapacity
+  // starting each index with value 0
+  var maxValuesAtCapacities = [];
+  for (var i = 0; i <= weightCapacity; i++) {
+      maxValuesAtCapacities[i] = 0;
+  }
+
+  for (var currentCapacity = 0; currentCapacity <= weightCapacity; currentCapacity++) {
+
+      // set a variable to hold the max monetary value so far for currentCapacity
+      var currentMaxValue = 0;
+
+      // we use a for loop here instead of forEach because we return infinity
+      // if we get a cakeType that weighs nothing and has a value. but forEach
+      // loops always return undefined and you can't break out of them without
+      // throwing an exception
+      for (var j = 0; j < cakeTypes.length; j++) {
+          var cakeType = cakeTypes[j];
+
+          // if a cake weighs 0 and has a positive value the value of our duffel bag is infinite!
+          if (cakeType.weight === 0 && cakeType.value !== 0) {
+              return Infinity;
+          }
+
+          // if the current cake weighs as much or less than the current weight capacity
+          // it's possible taking the cake would give get a better value
+          if (cakeType.weight <= currentCapacity) {
+
+              // so we check: should we use the cake or not?
+              // if we use the cake, the most kilograms we can include in addition to the cake
+              // we're adding is the current capacity minus the cake's weight. we find the max
+              // value at that integer capacity in our array maxValuesAtCapacities
+              var maxValueUsingCake = cakeType.value + maxValuesAtCapacities[currentCapacity - cakeType.weight];
+
+              // now we see if it's worth taking the cake. how does the
+              // value with the cake compare to the currentMaxValue?
+              currentMaxValue = Math.max(maxValueUsingCake, currentMaxValue);
+          }
+      }
+
+      // add each capacity's max value to our array so we can use them
+      // when calculating all the remaining capacities
+      maxValuesAtCapacities[currentCapacity] = currentMaxValue;
+  }
+
+  return maxValuesAtCapacities[weightCapacity];
+}
